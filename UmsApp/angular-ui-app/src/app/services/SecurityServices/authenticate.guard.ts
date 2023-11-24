e815636a7fa25bf1c6b9bb711bd5a66b33f99fe2
Vishal Pawar <vshalofficial@gmail.com>
2023-11-24 17:10:36 +0530

    16

--- UmsApp/angular-ui-app/src/app/services/SecurityServices/authenticate.guard.ts
+++ UmsApp/angular-ui-app/src/app/services/SecurityServices/authenticate.guard.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { CookieService } from 'ngx-cookie-service';
@@ -13,7 +7,7 @@ import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthenticateGuard implements CanActivate {
+export class AuthenticateGuard  {
   constructor(private router: Router, private authService: AuthService, private cookieService: CookieService) {}
 
   canActivate(
