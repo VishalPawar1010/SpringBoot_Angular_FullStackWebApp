cad2b0f0108eab5e4684865cad5126b7e9f05ae2
Vishal Pawar <vshalofficial@gmail.com>
2024-02-02 13:11:30 +0530

    UMS-40 : second check

--- UmsApp/nodeJS-backend-app/index.js
+++ UmsApp/nodeJS-backend-app/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
